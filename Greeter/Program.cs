1a13de26d24c1cf2ec3962c7ffd95ca1cb587bdc
juplozano <juanpulgalozano@gmail.com>
2021-09-15 15:26:59 -0500

    Fixing solution

---
+++ Greeter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Greeter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
